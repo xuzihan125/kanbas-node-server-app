463920b3764b912ac467465f4c0909112140a025
Zihan Xu <xuzihan125@163.com>
2024-04-14 14:59:50 -0700

    a6-1

--- app.js
+++ app.js
@@ -5,13 +5,43 @@ import Lab5 from "./lab5.js";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import cors from "cors";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+// mongoose.connect("mongodb://127.0.0.1:27017/Kanbas");
+import session from "express-session";
+import "dotenv/config";
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/Kanbas";
+mongoose.connect(CONNECTION_STRING);
 const app = express()
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+  },
+    // {
+    //     credentials: true,
+    //     origin: "http://localhost:3001"
+    // }
+));
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+    };
+}
+app.use(session(sessionOptions));
 CourseRoutes(app);
 ModuleRoutes(app);
 app.use(express.json());
 // app.get('/hello', (req, res) => {res.send('Life is good!')})
 // app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
+UserRoutes(app);
 Hello(app);
 Lab5(app);
 app.listen(4000)
